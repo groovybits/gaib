804154d8e20d6befcc6a5fa79f4d6f0175c18dbf
Chris Kennedy <chris.kennedy@crunchyroll.com>
2023-04-02 23:45:53 -0700

    set namespace in .env
    different instances have different namespaces

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -6,8 +6,12 @@ if (!process.env.PINECONE_INDEX_NAME) {
   throw new Error('Missing Pinecone index name in .env file');
 }
 
+if (!process.env.PINECONE_NAME_SPACE) {
+  throw new Error('Missing Pinecone name space in .env file');
+}
+
 const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
 
-const PINECONE_NAME_SPACE = 'groovypdf'; //namespace is optional for your vectors
+const PINECONE_NAME_SPACE = process.env.PINECONE_NAME_SPACE ?? '';;
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
