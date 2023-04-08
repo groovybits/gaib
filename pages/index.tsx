7809caf4af410e1ed1281a11c55bec093653d607
Chris Kennedy <chris.kennedy@crunchyroll.com>
2023-04-08 03:30:05 -0700

    fix regressions

--- pages/index.tsx
+++ pages/index.tsx
@@ -347,6 +347,28 @@ export default function Home() {
                   <div className={styles.buttonWrapper}>
                     <div className={styles.buttoncontainer}>
                       <button
+                        type="submit"
+                        disabled={loading}
+                        className={styles.generatebutton}
+                      >
+                        {loading ? (
+                          <div className={styles.loadingwheel}>
+                            <LoadingDots color="#FFA500" />
+                          </div>
+                        ) : (
+                          // Send icon SVG in input field
+                          <svg
+                            viewBox="0 0 20 20"
+                            className={styles.svgicon}
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                          </svg>
+                        )}
+                      </button>
+                    </div>
+                    <div className={styles.buttoncontainer}>
+                      <button
                         type="button"
                         disabled={loading}
                         className={`${styles.voicebutton} ${listening ? styles.listening : ''}`}
