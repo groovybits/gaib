93840e0be693758943c09e3f2f62a5ae4b5b9d90
Chris Kennedy <chris.kennedy@crunchyroll.com>
2023-05-02 00:59:58 -0700

    fix case issue

---
+++ components/Layout.tsx
@@ -0,0 +1,17 @@
+interface LayoutProps {
+  children?: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="mx-auto flex flex-col space-y-4">
+      <header className="container sticky top-0 z-40 bg-#FEC601">
+      </header>
+      <div>
+        <main className="flex w-full flex-1 flex-col overflow-hidden">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
