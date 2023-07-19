c653d075707aa3fc112b71f1c2e182d85ff1449a
Chris Kennedy <c@groovy.org>
2023-07-19 03:15:48 -0700

    fix twitch chatbot
    working chatbot! now to wire up into app sessions...

--- scripts/twitchChat.ts
+++ scripts/twitchChat.ts
@@ -1,46 +1,51 @@
-import WebSocket from 'ws';
+import tmi from 'tmi.js';
 import admin from 'firebase-admin';
 
 // Initialize Firebase Admin SDK
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault()
-  });
-}
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  }),
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+});
 
 const db = admin.firestore();
 
 // Get the channel name from the command line arguments
 const channelName = process.argv[2];
+const oAuthToken = process.env.TWITCH_OAUTH_TOKEN ? process.env.TWITCH_OAUTH_TOKEN : '';
 
-// Create a WebSocket connection to the Twitch PubSub system
-const twitchWs = new WebSocket('wss://pubsub-edge.twitch.tv');
+console.log(`channel ${channelName} with oAuth: ${oAuthToken}`);
 
-twitchWs.on('open', () => {
-  // Send a LISTEN request with your topics and authentication
-  twitchWs.send(JSON.stringify({
-    type: 'LISTEN',
-    data: {
-      topics: ['chat_moderator_actions.<userID>.<channelName>'],
-      auth_token: '<Your OAuth Token>'
-    }
-  }));
+// Create a TMI client
+const client = new tmi.Client({
+  options: { debug: true },
+  connection: {
+    secure: true,
+    reconnect: true
+  },
+  identity: {
+    username: channelName,
+    password: `oauth:${oAuthToken}`
+  },
+  channels: [channelName]
 });
 
-twitchWs.on('message', (data: any) => {
-  // Parse the incoming message
-  const message = JSON.parse(data.toString());
+client.connect();
 
-  // Check if it's a chat message
-  if (message.type === 'MESSAGE') {
-    // Parse the chat message
-    const chatMessage = message.data.message;
+client.on('message', (channel: any, tags: any, message: { startsWith: (arg0: string) => any; slice: (arg0: number) => { (): any; new(): any; split: { (arg0: string): [any, any]; new(): any; }; }; }, self: any) => {
+  // Ignore messages from the bot itself
+  if (self) return;
 
-    // Check if it's a command
-    if (chatMessage.startsWith('!episode:')) {
-      // Parse the title and plotline from the command
-      const [title, plotline] = chatMessage.slice(9).split(' - ');
+  // Check if the message is a command
+  if (message.startsWith('!episode:')) {
+    // Parse the title and plotline from the command
+    const [title, plotline] = message.slice(9).split(' - ');
 
+    // Check if both title and plotline are defined
+    if (title && plotline) {
       // Add the command to Firestore
       const docRef = db.collection('commands').doc();
       docRef.set({
@@ -50,6 +55,9 @@ twitchWs.on('message', (data: any) => {
         plotline,
         timestamp: admin.firestore.FieldValue.serverTimestamp()
       });
+    } else {
+      console.log('Invalid command format. Use: !episode: title - plotline');
     }
   }
 });
+
