ba62241bec9f2f075231a633d27a44dc7a9338e8
Chris Kennedy <c@groovy.org>
2024-01-21 08:23:10 -0800

    add updates

--- scripts/twitchChat.ts
+++ scripts/twitchChat.ts
@@ -23,17 +23,23 @@ const sanitizeInput = (input: string): string => {
 const channelName = process.argv[2];
 const oAuthToken = process.env.TWITCH_OAUTH_TOKEN ? process.env.TWITCH_OAUTH_TOKEN : '';
 const llmHost = process.env.LLM_HOST ? process.env.LLM_HOST : 'earth:8080';
-const chatHistorySize: number = process.env.TWITCH_CHAT_HISTORY_SIZE ? parseInt(process.env.TWITCH_CHAT_HISTORY_SIZE) : 2;
+const chatHistorySize: number = process.env.TWITCH_CHAT_HISTORY_SIZE ? parseInt(process.env.TWITCH_CHAT_HISTORY_SIZE) : 30;
 const maxTokens = 80;
 const temperature = 1.0;
 const openApiKey: string = "FAKE_API_KEY";
 const maxHistoryBytes = 4096;
-const twitchUserName = process.env.TWITCH_USER_NAME ? process.env.TWITCH_USER_NAME : 'llm';
+const twitchUserName = process.env.TWITCH_USER_NAME ? process.env.TWITCH_USER_NAME : 'moderator';
+const twitchModName = process.env.TWITCH_MOD_NAME ? process.env.TWITCH_MOD_NAME : 'buddha';
+const dominantBot = process.env.TWITCH_DOMINANT_BOT ? process.env.TWITCH_DOMINANT_BOT : 0;
 
-let lastMessageArray: any[] = [];
 const processedMessageIds: { [id: string]: boolean } = {};
+
 const howto: string = "Type !help to see the commands. Use !message <personality> <message> to ask a question, and !personalities to see the available personalities.";
-const personalityPrompt: string = `You are Mickey Mouse in your Buddhist Chatroom where you and Buddha moderate the chatroom and help users with their questions. Carry on conversations that are short with Buddha and the Chat room members. ${howto}`;
+
+const personalityPrompt: string = `You are ${twitchUserName} in your ${channelName} Chatroom where you and ${twitchModName} moderate the chatroom and help users with their questions. Carry on conversations that are short with ${twitchModName} and the Chat room members. These are the instructions to tell users for help using the chat, ${twitchModName} already knows these: ${howto}`;
+let lastMessageArray: any[] = [];
+
+console.log(`Using prompt: ${personalityPrompt}`)
 
 if (!channelName) {
   console.log('Usage: node twitchChat.js <channelName>');
@@ -46,6 +52,20 @@ if (!oAuthToken) {
 }
 
 console.log(`channel ${channelName} starting up with user ${twitchUserName}...`);
+
+// Delay function
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Function to generate a random delay between 5 and 120 seconds
+function getRandomDelay() {
+  // Generate a random number between 30 and 90
+  const randomDelay = Math.floor(Math.random() * (90 - 30 + 1)) + 5;
+
+  // Convert the delay to milliseconds
+  return randomDelay * 1000;
+}
 
 // Create a TMI client
 const client = new tmi.Client({
@@ -63,10 +83,10 @@ const client = new tmi.Client({
 
 client.connect();
 
-// Yoda's wisdom: A mapping to keep track of the last message timestamp for each user
+// A mapping to keep track of the last message timestamp for each user
 const lastMessageTimestamps: { [username: string]: number } = {};
 
-// Yoda's wisdom: Function to check for user inactivity
+// Function to check for user inactivity
 const checkUserInactivity = () => {
   const currentTime = Date.now();
   Object.keys(lastMessageTimestamps).forEach((username) => {
@@ -76,50 +96,54 @@ const checkUserInactivity = () => {
   });
 };
 
-// Yoda's wisdom: Periodically check for user inactivity
+// Periodically check for user inactivity
 setInterval(checkUserInactivity, 30000);  // Run every 30 seconds
 
 // Store usernames initially present in the room
 let initialUsers: Set<string> = new Set();
-let newUsers: Set<string> = new Set();  // Yoda's wisdom: A new set for users joining after initialization
+let newUsers: Set<string> = new Set();  // A new set for users joining after initialization
 let hasInitialized: boolean = false;
 
-// Yoda's wisdom: Delay the initialization to ensure the bot has connected and received the initial 'join' events
-setTimeout(() => {
-  hasInitialized = true;
-}, 60000);  // 60-second delay
+let startDate = new Date();
 
-// Yoda's wisdom: Welcome only new users when they join the room
+// Join the channel
 client.on('join', (channel: any, username: any, self: any) => {
   if (self) return;  // Ignore messages from the bot itself
 
-  // Yoda's wisdom: If the bot has initialized, and the user is new, welcome them
+  // If the bot has initialized, and the user is new, welcome them
   if (hasInitialized && !initialUsers.has(username) && !newUsers.has(username)) {
-    //client.say(channel, `Welcome to the channel, ${username}! Use !message <personality> <message> to ask a question, and !personalities to see the available personalities.`);
-    newUsers.add(username);  // Yoda's wisdom: Add the user to the newUsers set
+    // check if startDate and current date are  more than 3 minutes apart
+    let currentDate = new Date();
+    let timeDiff = Math.abs(currentDate.getTime() - startDate.getTime());
+    let diffMinutes = Math.ceil(timeDiff / (1000 * 60));
+    if (diffMinutes > 3) {
+      if (dominantBot > 0) {
+        client.say(channel, `  Welcome to the channel, ${username}! Use !message <personality> <message> to ask a question, and !personalities to see the available personalities.`);
+      }
+    }
+    newUsers.add(username);  // Add the user to the newUsers set
 
-    // Yoda's wisdom: Set the last message timestamp for this user upon joining
+    // Set the last message timestamp for this user upon joining
     lastMessageTimestamps[username] = Date.now();
   }
 
-  // Yoda's wisdom: Add username to the initialUsers set to avoid welcoming again
+  // s Add username to the initialUsers set to avoid welcoming again
   if (!hasInitialized) {
     initialUsers.add(username);
 
-    // Yoda's wisdom: Set the last message timestamp for this user upon joining
+    // Set the last message timestamp for this user upon joining
     lastMessageTimestamps[username] = Date.now();
   }
 });
 
-lastMessageArray.push({ "role": "system", "content": personalityPrompt });
-
+// message handler
 client.on('message', async (channel: any, tags: {
   id: any; username: any;
 }, message: any, self: any) => {
   // Ignore messages from the bot itself
   if (self) return;
-
-  message = sanitizeInput(message);
+  console.log(`Username: ${tags.username} Message: ${message} with twitchUserName is ${twitchUserName}`)
+  if (tags.username.toLowerCase() === twitchUserName.toLowerCase()) return;
 
   // Ignore messages that have already been processed
   if (processedMessageIds[tags.id]) {
@@ -127,7 +151,7 @@ client.on('message', async (channel: any, tags: {
     return;
   }
 
-  // Yoda's wisdom: Update the last message timestamp for this user
+  // Update the last message timestamp for this user
   if (tags.username) {
     lastMessageTimestamps[tags.username] = Date.now();
   }
@@ -135,26 +159,16 @@ client.on('message', async (channel: any, tags: {
   // Mark this message as processed
   processedMessageIds[tags.id] = true;
 
-  // remove the oldest message from the array
-  if (lastMessageArray.length > chatHistorySize) {
-    // don't remove the oldest message if it is a system message, then remove the one after it
-    if (lastMessageArray[0].role === 'system') {
-      lastMessageArray.splice(1, 1);
-    } else {
-      lastMessageArray.shift();
+  // confirm the maxHistoryBytes is not exceeded, only remove messages after the oldest one and measure how many bytes are left till we are below this value
+  let historyBytes = Buffer.byteLength(JSON.stringify(lastMessageArray));
+  if (historyBytes > maxHistoryBytes) {
+    let i = 0;
+    while (historyBytes > maxHistoryBytes) {
+      historyBytes = Buffer.byteLength(JSON.stringify(lastMessageArray.slice(i)));
+      i++;
     }
+    lastMessageArray = lastMessageArray.slice(i);
   }
-
-  // remove any messages after the maxHistoryBytes are met
-  let historyBytes = 0;
-  let historyIndex = 0;
-  lastMessageArray.forEach((messageObject: any) => {
-    historyBytes += JSON.stringify(messageObject).length;
-    if (historyBytes > maxHistoryBytes) {
-      lastMessageArray.splice(historyIndex, lastMessageArray.length - historyIndex);
-    }
-    historyIndex++;
-  });
 
   // structure tohold each users settings and personality prompts
   const userSettings: any = {};
@@ -166,36 +180,53 @@ client.on('message', async (channel: any, tags: {
     }
     userSettings[tags.username].lastMessageTimestamp = Date.now();
   }
-  console.log(`Received message: ${message}\nfrom ${tags.username} in ${channel}\nwith tags: ${JSON.stringify(tags)}\n`)
+  console.log(`Received message: ${message}\nfrom ${tags.username} in channel ${channel}\nwith tags: ${JSON.stringify(tags)}\n`)
 
+  // array of key words to respond to
+  let is_mentioned = true;
+  const keywords = ['help', 'question', 'how', 'why', 'need', 'want', 'who', 'where', 'when', 'get'];
+  if (keywords.some((keyword) => message.toLowerCase().includes(keyword))) {
+    is_mentioned = true;
+  }
+
+  // Dominant bot
+  if (dominantBot > 0) {
+    if (!message.toLowerCase().includes(twitchModName.toLowerCase())) {
+      is_mentioned = true;
+    }
+  }
+
+  // check if we are a mod
+  if (tags.username.toLowerCase() !== twitchModName.toLowerCase()) {
+  }
+
+  // check if we were mentioned in the message
+  if (message.toLowerCase().includes(twitchUserName.toLowerCase())) {
+    is_mentioned = true;
+  }
 
   // check message to see if it is a command
-  if (message.startsWith('!')) {
+  // compare name lowercase to message lowercase
+  if (message.startsWith('!') || !is_mentioned) {
+    console.log(`Skipping message: ${message} from ${tags.username} in channel ${channel} with tags: ${JSON.stringify(tags)}\n`)
     // Do nothing
-    console.log(`Ignoring command message: ${message}`);
   } else {
     let promptArray: any[] = [];
+    promptArray.push({ "role": "system", "content": personalityPrompt });
     // copy lastMessageArray into promptArrary prepending the current content member with the prompt variable
-    let counter = 0;
     let gptAnswer = '';
-
-    let isStory = false;
-    // parse the message to see if it is a question or eipsode request, as a fuzzy nlp type match, where it is free form english chat, it may be called a story too
-    if (allowStories && message.toLowerCase().includes('episode')) {
-      isStory = true;
-    }
 
     lastMessageArray.forEach((messageObject: any) => {
       if (messageObject.role && messageObject.content) {
         promptArray.push({ "role": messageObject.role, "content": messageObject.content });
       }
-      counter++;
     });
     // add the current message to the promptArray with the final personality prompt
-    promptArray.push({ "role": "user", "content": `You have a new message from ${tags.username} who said ${message}. Please respond to them by name.` });
-    promptArray.push({ "role": "assistant", "content": "" });
+    promptArray.push({ "role": "user", "content": `${tags.username} said ${message}.` });
+    promptArray.push({ "role": "assistant", "content": `` });
+
     // save the last message in the array for the next prompt
-    lastMessageArray.push({ "role": "user", "content": `${tags.username} asked ${message}` });
+    lastMessageArray.push({ "role": "user", "content": `${tags.username} said ${message}` });
 
     console.log(`OpenAI promptArray:\n${JSON.stringify(promptArray, null, 2)}\n`);
 
@@ -226,7 +257,6 @@ client.on('message', async (channel: any, tags: {
         if (data.choices && data.choices.length > 0 && data.choices[0].message && data.choices[0].message.content) {
           const aiMessage = data.choices[0].message;
           console.log(`OpenAI response:\n${JSON.stringify(aiMessage, null, 2)}\n`);
-
           console.log(`OpenAI usage:\n${JSON.stringify(data.usage, null, 2)}\nfinish_reason: ${data.choices[0].finish_reason}\n`);
 
           gptAnswer = aiMessage.content;
@@ -234,21 +264,19 @@ client.on('message', async (channel: any, tags: {
           if (gptAnswer.startsWith('!')) {
             gptAnswer = gptAnswer.substring(1);
           }
-          client.say(channel, aiMessage.content);
+          client.say(channel, `  ${aiMessage.content}`);
 
-          lastMessageArray.push(aiMessage);
+          lastMessageArray.push({ "role": "assistant", "content": `${aiMessage.content}` });
         } else {
           console.error('No choices returned from OpenAI!\n');
           console.log(`OpenAI response:\n${JSON.stringify(data)}\n`);
         }
       })
       .catch(error => console.error('An error occurred:', error));
-    // sleep for 30 seconds to allow the ai to respond
-    // generate random delay between 30 and 90 seconds for timeout
-    const delay = Math.floor(Math.random() * 60) + 30;
-    setTimeout(() => {
-      hasInitialized = true;
-    }, delay);  // N-second delay
+
+    // Introduce a random delay before the bot responds
+    await delay(getRandomDelay());
+
     return;
   }
 });
